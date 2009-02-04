b980ce2c9ef927c27aad677c4781dc474c6fd901
hari <harijay@gmail.com>
2009-02-04 13:08:45 -0500

    Fixed mistakes in example4 and also did ph Gradients

--- example5.py
+++ example5.py
@@ -10,6 +10,7 @@ Copyright (c) 2009 __MyCompanyName__. All rights reserved.
 import sys
 import os
 import plate,masterplate,component
+import buffercomponent
 
 def main():
 	# Create the plate with volume of 2 mls or 2000 microlitre : volume should be in microlitres ( eg 2000 µl)
@@ -32,18 +33,25 @@ def main():
 	p3.gradient_along_x(mp,peg400,18,25)
 	p4.gradient_along_x(mp,peg400,18,25)
 	
-	# Define the four buffers that will be added along the alphabet axes 
-	b1 = component.Component("ph7p6",1000,100000)
-	b2 = component.Component("ph8p0",1000,100000)
-	b3 = component.Component("ph8p2",1000,100000)
-	b4 = component.Component("ph8p4",1000,100000)
+	# Define the buffer components  that will be mixed to get the pH gradients along the alphabet axes 
+	# Buffer components that are used for mixing can be defined as follows after importing the buffercomponent.SimpleBuffer
+	# buffer = buffercomponent.SimpleBuffer(name,stockconc,totalvol,ph,pka)
 	
-	# Now these buffers will be the same for an entire row across the plates we could do this as done below or as given in example 1
-	# define a plate that corresponds to the whole 96 wells
-	pwhole = plate.Plate("A1","H12",mp)
+	b1 = buffercomponent.SimpleBuffer("ph7p5",1,100000,7.5,8.03)
+	b2 = buffercomponent.SimpleBuffer("ph8.5",1,100000,8.5,8.03)
 	
-	# Use this to add the components using the plate methods push_components_mapped_to_row, Note that the pattern is specified according to the order 
-	pwhole.push_components_mapped_to_row(mp,[b1,b2,b3,b4,b1,b2,b3,b4],[100,100,100,100,100,100,100,100],["A","B","C","D","E","F","G","H"])
+	# Now we will setup the same gradient as we did before as specified gradient 7.6 , 8.0 ,8.2 , 8.4
+	# The methods for buffers are
+	# ph_gradient_alongx(masterplate,buffer1,buffer2,finalconc,startph,stopph)
+	# ph_gradient_alongy(masterplate,buffer1,buffer2,finalconc,startph,stopph)
+	# ph_list_alongx(masterplate,buffer1,buffer2,finalconc,phlist)
+	# ph_list_alongy(masterplate,buffer1,buffer2,finalconc,phlist)
+	# Note that Buffer stock conc is specified in molar i.e 1 so the final conc is also in molar 0.1
+	p.ph_list_alongy(mp,b1,b2,0.1,[7.6,8.0,8.2,8.4])
+	p2.ph_list_alongy(mp,b1,b2,0.1,[7.6,8.0,8.2,8.4])
+	p3.ph_list_alongy(mp,b1,b2,0.1,[7.6,8.0,8.2,8.4])
+	p4.ph_list_alongy(mp,b1,b2,0.1,[7.6,8.0,8.2,8.4])
+	
 	
 	
 	# We want each quadrant ( i.e plate p , p2 , p3 and p4 ) to have a constant concentration of Calcium Chloride
@@ -57,13 +65,17 @@ def main():
 	
 	# Now its time to fill the rest with water calling the fill_water method. We can do it for whole plate using pwhole defined from ( A1 to H12)
 	# The water concentration is arbitratrily assigned to a value. Has no implications on dispensed volume
+	# We will define pwhole for the water filling step since it has to take place for whole plate
+	
+	pwhole = plate.Plate("A1","H12",mp)
 	water = component.Component("100.00% Water",1000,300000)
+	
 	pwhole.fill_water(mp,water)
 	# You can output the composition of each well to stdout using the printwellinfo() method of masterplate.Masterplate class
 	mp.printwellinfo()
 	
 	# The makefilefor formulatrix outputs the final dispense list
-	mp.makefileforformulatrix("example4.dl.txt")
+	mp.makefileforformulatrix("example5.dl.txt")
 	
 
 if __name__ == '__main__':
